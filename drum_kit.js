db81f4a3c6d3b3cec86891faeb5299049fe66054
Lakshay Kumar <imlakshaykumar@gmail.com>
2023-01-19 10:45:04 +0530

    JavaScript file

---
+++ drum_kit.js
@@ -0,0 +1,23 @@
+function playSound(e) {
+  // console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function from running all together
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  // console.log(audio);
+  // console.log(key);
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  // console.log(e);
+  if (e.propertyName !== 'transform') return; //skip it if it's not a transform
+  console.log(e.propertyName);
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
